614b4c596697e9f3d3d27ce799866c1acdf8d11e
Alex <alexbilozir@gmail.com>
2022-11-15 22:55:31 +0400

    upd

--- dapp.config.js
+++ dapp.config.js
@@ -7,7 +7,7 @@ const config = {
   contractAddress: '0x4f2E29c029b5c2ab4c17Ee41793b116d72985CA7',
   maxMintAmount: 10,
   presaleMaxMintAmount: 2,
-  Price: 0.02,
+  Price: 0.0069,
   presalePrice: 0
 }
 
